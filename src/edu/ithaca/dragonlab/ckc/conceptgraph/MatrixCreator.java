13dc711cd2d35d4de079191cce9b097c58e3a826
bleblanc2 <bleblanc2@ithaca.edu>
2017-06-13 16:11:44 -0400

    starting MatrixCreator and MatrixCreatorTest()

---
+++ src/edu/ithaca/dragonlab/ckc/conceptgraph/MatrixCreator.java
@@ -0,0 +1,28 @@
+package edu.ithaca.dragonlab.ckc.conceptgraph;
+
+import java.io.File;
+
+/**
+ * Created by bleblanc2 on 6/13/17.
+ */
+public class MatrixCreator {
+
+    String id;
+    double[][] structure;
+
+    public MatrixCreator(String id){
+        this.id = id;
+        this.structure = structure;
+    }
+
+    public static double[][] createMatrix(String matrixString){
+
+
+    }
+
+    public static double[][] createMatrix(File csvfile){
+
+
+    }
+
+}
