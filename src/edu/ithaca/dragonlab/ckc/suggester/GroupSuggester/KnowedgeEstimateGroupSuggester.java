946c024f005e7ba1f92e1c83a46ef89f0cbfaabf
Mia Kimmich Mitchell <ekimmichmitchell@ithaca.edu>
2017-08-05 14:47:27 -0400

    added to graph suggestions

--- src/edu/ithaca/dragonlab/ckc/suggester/GroupSuggester/KnowedgeEstimateGroupSuggester.java
+++ src/edu/ithaca/dragonlab/ckc/suggester/GroupSuggester/KnowedgeEstimateGroupSuggester.java
@@ -24,16 +24,27 @@ public class KnowedgeEstimateGroupSuggester extends GroupSuggester{
 
 
 
-//        for(String name: getMaps.keySet()){
-//            ConceptGraph userGraph = getMaps.get(name);
-//
-//            double sum = getSum(userGraph);
-//
-//            System.out.println(name + " " + sum);
-//
-//            knowledgeSums.put(name, sum);
-//
-//        }
+        for(String name: getMaps.keySet()){
+            ConceptGraph userGraph = getMaps.get(name);
+
+            double sum = getSum(userGraph);
+
+            System.out.println(name + " " + sum);
+
+            knowledgeSums.put(name, sum);
+
+        }
+
+        //create groups with the most similar users
+        //groups will go from most similar to least similar
+        List<List<String>> groupings = new ArrayList<>();
+
+
+        Map<String, Double> tempMap = new HashMap<>();
+        tempMap.putAll(knowledgeSums);
+
+
+
 
         //testing
         Collection<ConceptGraph> lsit = getMaps.values();
@@ -41,83 +52,77 @@ public class KnowedgeEstimateGroupSuggester extends GroupSuggester{
         list.addAll(lsit);
 
 
-//        double sum1 = getSum(list.get(0));
-//        System.out.println("TEST " + sum1);
-//
 
 
-//        System.out.println(list.get(0).getName());
+        Map<String, ConceptNode> map = list.get(0).getNodeMap();
+        double sum =0;
+        for(ConceptNode node: map.values()){
 
-//        Map<String, ConceptNode> map = list.get(0).getNodeMap();
-//        double sum =0;
-//        for(ConceptNode node: map.values()){
-//
-//            sum = sum+ node.getKnowledgeEstimate();
-//
-//        }
-//
-//        System.out.println("ACTUAL " + sum);
+            sum = sum+ node.getKnowledgeEstimate();
 
-        knowledgeSums.put("s1", 0.84483833234);
-        knowledgeSums.put("s2", .7823423);
-        knowledgeSums.put("s3", .123000000);
-        knowledgeSums.put("s4", .5767834534);
+        }
 
+        System.out.println("ACTUAL " + sum);
 
-        //create groups with the most similar users
-        //groups will go from most similar to least similar
-        List<List<String>> groupings = new ArrayList<>();
-
-        double temp = 1 ;
-        String st1 = "";
-        String st2 = "";
-
-        Map<String, Double> tempMap = new HashMap<>();
-        tempMap.putAll(knowledgeSums);
+//        knowledgeSums.put("s1", 0.84483833234);
+//        knowledgeSums.put("s2", .7823423);
+//        knowledgeSums.put("s3", .123000000);
+//        knowledgeSums.put("s4", .5767834534);
+//        knowledgeSums.put("s5", .3498273942);
+//        knowledgeSums.put("s6", .12312324);
 
 
-        List<String> repeats = new ArrayList<>();
+        //this is for groups of 2
+
+        while(groupings.size() != getMaps.size()/choice) {
+            double temp = 1 ;
+            String st1 = "";
+            String st2 = "";
+
+            List<String> repeats = new ArrayList<>();
+
+            for (String name : tempMap.keySet()) {
+                double sum1 = tempMap.get(name);
+
+                for (String name2 : tempMap.keySet()) {
+                    double sum2 = tempMap.get(name2);
+
+                    if (!name.equals(name2)) {
+
+                        if (!name.equals(name2) && !(repeats.contains(name + "+" + name2) || repeats.contains(name2 + "+" + name))) {
 
 
-        for(String name: tempMap.keySet()){
-            double sum = tempMap.get(name);
+                            if ((Math.abs(sum1 - sum2)) < temp) {
+                                temp = (Math.abs(sum1 - sum2));
+                                st1 = name;
+                                st2 = name2;
 
-            for(String name2: tempMap.keySet()){
-                double sum2 = tempMap.get(name2);
+                            }
 
-                if(!name.equals(name2)){
-
-                    if(!name.equals(name2) && !(repeats.contains(name+"+"+name2) || repeats.contains(name2+"+"+name))) {
+                            repeats.add(name + "+" + name2);
 
 
-//
-                    if((Math.abs(sum- sum2))< temp){
-                        temp = (Math.abs(sum- sum2));
-                        st1= name;
-                        st2 = name2;
-
-                    }
-
-                        repeats.add(name+"+"+name2);
-
+                        }
                     }
                 }
             }
 
+            List<String> group = new ArrayList<>();
+            group.add(st1);
+            group.add(st2);
+
+            tempMap.remove(st1, tempMap.get(st1));
+            tempMap.remove(st2, tempMap.get(st2));
+
+
+            groupings.add(group);
 
         }
 
-
-        System.out.println(temp);
-        System.out.println(st1 + " " + st2);
+        System.out.println(groupings);
 
 
-
-
-
-
-
-        return null;
+        return groupings;
 
     }
 
