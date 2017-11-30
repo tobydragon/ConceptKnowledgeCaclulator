1a05aae7fad2b067e33c1aa376ce9b8d93d85b8a
Mia <ekimmichmitchell@ithaca.edu>
2017-11-29 20:40:20 -0500

    x

---
+++ src/edu/ithaca/dragonlab/ckc/suggester/GroupSuggester/ComplementaryKnowledgeSuggester.java
@@ -0,0 +1,25 @@
+package edu.ithaca.dragonlab.ckc.suggester.GroupSuggester;
+
+import java.util.List;
+
+/**
+ * Created by Mia Kimmich Mitchell on 11/28/2017.
+ */
+public class ComplementaryKnowledgeSuggester extends Suggester{
+
+
+    //is complementary(Concept node, concept node)
+    //returns boolean
+
+    @Override
+    public List<Group> suggestGroup(Group groupSoFar, Group extraMembers) {
+
+//        for(Group groupSoFar: groupSoFar.
+
+
+
+        return null;
+
+    }
+
+}
