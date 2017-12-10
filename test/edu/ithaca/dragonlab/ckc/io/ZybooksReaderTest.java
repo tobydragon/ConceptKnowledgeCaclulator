9dae006e17ac0a4c86f754ef77d6662a72a87bcd
Ryan DeBolt <ryandebolt1@gmail.com>
2017-12-10 18:26:58 -0500

    tests organized

--- test/edu/ithaca/dragonlab/ckc/io/ZybooksReaderTest.java
+++ test/edu/ithaca/dragonlab/ckc/io/ZybooksReaderTest.java
@@ -10,9 +10,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import edu.ithaca.dragonlab.ckc.learningobject.ManualGradedResponse;
 
-import static edu.ithaca.dragonlab.ckc.io.ReaderTools.lineToList;
-import static edu.ithaca.dragonlab.ckc.io.ReaderTools.pullNumber;
-
 /**
  * Created by rdebolt on 10/23/18.
  */
@@ -20,54 +17,7 @@ import static edu.ithaca.dragonlab.ckc.io.ReaderTools.pullNumber;
 // make pull request from dev to commaTest
 public class ZybooksReaderTest {
     //ReaderTools toolBox = new ReaderTools();
-    @Test
-    public void NumbersTest(){
-        String object = "total Score - 25";
-        String outCome = "25";
-        try {
-            Assert.assertEquals(outCome,ReaderTools.pullNumber(object));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
-    @Test
-    public void NumbersTestReturnEmpty(){
-        String object = "total Score - 25.3.2";
-        String outCome = "";
-        try {
-            Assert.assertEquals(outCome,ReaderTools.pullNumber(object));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
-    @Test
-    public void NumbersTestDouble(){
-        String object = "total Score - -25.34";
-        String outCome = "-25.34";
-        try {
-            Assert.assertEquals(outCome,ReaderTools.pullNumber(object));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
-    @Test
-    public void NumbersTestNoNumbers(){
-        String object = "total Score - N/A";
-        String outCome = "";
-        try {
-            Assert.assertEquals(outCome,ReaderTools.pullNumber(object));
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
+
     @Test
     public void FullNameTest(){
         String file = "test/testresources/io/assessmentSources/zybook-comp115-DataExample.csv";
@@ -113,67 +63,6 @@ public class ZybooksReaderTest {
             Assert.fail();
         }
     }
-    @Test
-    public void titleCommasTest() {
-        String titles = "this is, a title to test. this, will not work";
-        List<String> myList = Arrays.asList("this is","a title to test. this","will not work");
-        try {
-            Assert.assertEquals(myList, lineToList(titles));
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
-    @Test
-    public void titleCommasTestTwo() {
-        String titles = "another test, to fail again, lets see how this goes";
-        List<String> myList = Arrays.asList("another test", "to fail again", "lets see how this goes");
-        try {
-            Assert.assertEquals(myList, lineToList(titles));
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
-    @Test
-    public void titleCommasTestThree() {
-        String titles = "hello, a third test, maybe \"this one will pass\". Probably not";
-        List<String> myList = Arrays.asList("hello", "a third test" , "maybe this one will pass. Probably not");
-        try {
-            Assert.assertEquals(myList, lineToList(titles));
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
-    @Test
-    public void titleStarTest () {
-        String titles = "hello, a \"fourth\" test, *Breaks here*";
-        List<String> myList = Arrays.asList("hello", "a fourth test" , "*Breaks here*");
-        try {
-            Assert.assertEquals(myList, lineToList(titles));
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
-    @Test
-    public void titleColonTest() {
-        String titles = "Test 5: another break, \"maybe, maybe-not?\"";
-        List<String> myList = Arrays.asList("Test 5: another break", "maybe, maybe-not?");
-        try {
-            Assert.assertEquals(myList, lineToList(titles));
-        }
-        catch(Exception e){
-            e.printStackTrace();
-            Assert.fail();
-        }
-    }
-
     @Test
     public void createQuestionsTest() {
         String file = "test/testresources/io/assessmentSources/zybook-comp115-DataExample.csv";
