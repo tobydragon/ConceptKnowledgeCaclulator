64d3f7ca33e1b2ace34f5f69625aebf7fee6020b
Toby Dragon <tdragon@ithaca.edu>
2018-06-04 11:19:20 -0400

    added files dattstore to main

--- src/main/java/edu/ithaca/dragon/tecmap/Settings.java
+++ src/main/java/edu/ithaca/dragon/tecmap/Settings.java
@@ -5,6 +5,8 @@ public class Settings {
     public static final String RESOURCE_DIR = "src/main/resources/author/";
     public static final String PRIVATE_RESOURCE_DIR = "src/main/resources/author/private";
     public static final String TEST_RESOURCE_DIR = "src/test/resources/author/";
+
+    public static final String DEFAULT_MAIN_DATASTORE_FILE = "src/main/resources/datastore/TecmapDataMain.json";
     public static final String DEFAULT_TEST_DATASTORE_FILE = "src/test/resources/datastore/TecmapTestExamples.json";
 
     public static final String WEB_JSON_DIR = "src/main/web/json/";
