d87107cb9c4439c983609aa533f1ab634eff06c1
apython1998 <alex.python1998@gmail.com>
2018-06-06 09:40:09 -0400

    Added new branch for the web error fixes

--- src/main/java/edu/ithaca/dragon/tecmap/ui/springbootui/service/TecmapService.java
+++ src/main/java/edu/ithaca/dragon/tecmap/ui/springbootui/service/TecmapService.java
@@ -19,6 +19,7 @@ public class TecmapService {
 
     public TecmapService() throws IOException{
         tecmapDatastore = TecmapFileDatastore.buildFromJsonFile(Settings.DEFAULT_MAIN_DATASTORE_FILE);
+//        tecmapDatastore = TecmapFileDatastore.buildFromJsonFile(Settings.DEFAULT_TEST_DATASTORE_FILE);
     }
 
     public TecmapService(TecmapDatastore tecmapDatastore) {
