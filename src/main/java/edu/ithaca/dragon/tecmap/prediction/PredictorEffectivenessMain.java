cc566b4fb41267eec532c9ceb5b000256af605b8
apython1998 <alex.python1998@gmail.com>
2018-07-02 14:01:12 -0400

    Refactored PredictorEffectivenessMain to make it easier to change out what classes are being analyzed

--- src/main/java/edu/ithaca/dragon/tecmap/prediction/PredictorEffectivenessMain.java
+++ src/main/java/edu/ithaca/dragon/tecmap/prediction/PredictorEffectivenessMain.java
@@ -16,31 +16,39 @@ public class PredictorEffectivenessMain {
 
     private static final Double[] learningSizeRatios = {0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9};
 
-    public static void main(String[] args) throws IOException {
-        //Test the Bayes Predictor Effectiveness on COMP220 Data
-        Predictor bayes = new BayesPredictor();
-
+    /**
+     * Gets graph from the main datastore assuming:
+     * assessment files are all in a private subfolder,
+     * resources files are all in the main folder and contain "Resources" in their name,
+     * there is one graph file and contains "Graph" in its name
+     * @param courseName the name of the directory in the datastore for the course
+     * @return ConceptGraph built from the graph file, the assessment files and resource files
+     */
+    private static ConceptGraph getConceptGraph(String courseName) throws IOException {
         //Get assessment filenames
-        File directory = new File(Settings.DEFAULT_MAIN_DATASTORE_PATH + "comp220Dragon/private/");
+        File directory = new File(Settings.DEFAULT_MAIN_DATASTORE_PATH + courseName + "/private/");
 
         List<File> files = Arrays.asList(directory.listFiles());
         List<String> assessmentFilenames = new ArrayList<>();
         for (File file : files) {
             if (file.isFile()) {
                 if (!file.getName().equals(".gitignore")) {
-                    assessmentFilenames.add(Settings.DEFAULT_MAIN_DATASTORE_PATH + "comp220Dragon/private/" + file.getName());
+                    assessmentFilenames.add(Settings.DEFAULT_MAIN_DATASTORE_PATH + courseName + "/private/" + file.getName());
                 }
             }
         }
 
-        //Get resources
-        directory = new File(Settings.DEFAULT_MAIN_DATASTORE_PATH + "comp220Dragon");
-        files = Arrays.asList(directory.listFiles());
         List<String> resourceFilenames = new ArrayList<>();
+        String graphFilename = null;
+        //Get resources
+        directory = new File(Settings.DEFAULT_MAIN_DATASTORE_PATH + courseName);
+        files = Arrays.asList(directory.listFiles());
         for (File file : files) {
             if (file.isFile()) {
                 if (file.getName().contains("Resources")) {
-                    resourceFilenames.add(Settings.DEFAULT_MAIN_DATASTORE_PATH + "comp220Dragon/" + file.getName());
+                    resourceFilenames.add(Settings.DEFAULT_MAIN_DATASTORE_PATH + courseName + "/" + file.getName());
+                } else if (file.getName().contains("Graph")) {
+                    graphFilename = Settings.DEFAULT_MAIN_DATASTORE_PATH + courseName + "/" + file.getName();
                 }
             }
         }
@@ -50,17 +58,27 @@ public class PredictorEffectivenessMain {
             loRecords.addAll(LearningResourceRecord.buildListFromJson(filename));
         }
 
-
-        ConceptGraph conceptGraph220 = new ConceptGraph(ConceptGraphRecord.buildFromJson(Settings.DEFAULT_MAIN_DATASTORE_PATH + "comp220Dragon/comp220DragonGraph.json"),
+        ConceptGraph courseGraph = new ConceptGraph(ConceptGraphRecord.buildFromJson(graphFilename),
                 loRecords,
                 AssessmentItemResponse.createAssessmentItemResponses(assessmentFilenames));
 
+        return courseGraph;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //Test the Bayes Predictor Effectiveness on COMP220 Data
+        Predictor bayes = new BayesPredictor();
+
+        ConceptGraph conceptGraph220 = getConceptGraph("comp220Dragon");
+
         LearningSetSelector baseLearningSetSelector = new BaseLearningSetSelector();
+        LearningSetSelector graphLearningSetSelector = new GraphLearningSetSelector();
 
         //Will get a null pointer exception when trying to predict an assessment not connected to a resource
         for (double ratio : learningSizeRatios) {
-            PredictorEffectiveness comp220Effectiveness = PredictorEffectiveness.testPredictor(bayes, baseLearningSetSelector,"final-1", conceptGraph220, ratio);
-            System.out.println("Learning Set Size: " + ratio + "\tPercent Correct: " + comp220Effectiveness.getPercentCorrect());
+            PredictorEffectiveness comp220BaseEffectiveness = PredictorEffectiveness.testPredictor(bayes, baseLearningSetSelector,"7-HG", conceptGraph220, ratio);
+            PredictorEffectiveness comp220GraphEffectiveness = PredictorEffectiveness.testPredictor(bayes, graphLearningSetSelector,"7-HG", conceptGraph220, ratio);
+            System.out.println("Learning Set Size: " + ratio + "\t Base Percent Correct: " + comp220BaseEffectiveness.getPercentCorrect() + "\t Graph Percent Correct: " + comp220GraphEffectiveness.getPercentCorrect());
         }
     }
 
