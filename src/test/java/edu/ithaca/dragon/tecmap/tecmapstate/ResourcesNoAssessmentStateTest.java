5683fe272acb1af1bf1a6966368ec61ea3c554ed
Toby Dragon <dr.toby.dragon@gmail.com>
2018-07-07 22:11:15 -0400

    fixed broken test

--- src/test/java/edu/ithaca/dragon/tecmap/tecmapstate/ResourcesNoAssessmentStateTest.java
+++ src/test/java/edu/ithaca/dragon/tecmap/tecmapstate/ResourcesNoAssessmentStateTest.java
@@ -8,8 +8,6 @@ import edu.ithaca.dragon.tecmap.io.record.LearningResourceRecord;
 import edu.ithaca.dragon.tecmap.tecmapExamples.Cs1ExampleJsonStrings;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
@@ -20,12 +18,12 @@ class ResourcesNoAssessmentStateTest {
     @Test
     public void getResourceRecordLinksTest() throws IOException {
         ResourcesNoAssessmentState state = new ResourcesNoAssessmentState(
-                new ConceptGraph(ConceptGraphRecord.buildFromJson(Settings.TEST_RESOURCE_DIR + "tecmapExamples/Cs1ExampleGraph.json")),
-                LearningResourceRecord.createLearningResourceRecordsFromJsonFiles(new ArrayList<>(Arrays.asList(Settings.TEST_RESOURCE_DIR + "tecmapExamples/Cs1ExampleResources.json")))
+                new ConceptGraph(ConceptGraphRecord.buildFromJson(Settings.DEFAULT_TEST_DATASTORE_PATH + "Cs1Example/Cs1ExampleGraph.json")),
+                LearningResourceRecord.createLearningResourceRecordsFromJsonFile(Settings.DEFAULT_TEST_DATASTORE_PATH + "Cs1Example/Cs1ExampleResources.json")
         );
 
         assertEquals(Cs1ExampleJsonStrings.resourcesConnectedString, Json.toJsonString(state.getResourceRecordLinks()));
-        assertEquals(Cs1ExampleJsonStrings.structureAsTreeString, Json.toJsonString(state.createStructureTree()));
+        assertEquals(Cs1ExampleJsonStrings.structureWithResourceConnectionsAsTree, Json.toJsonString(state.createStructureTree()));
     }
 
 }
