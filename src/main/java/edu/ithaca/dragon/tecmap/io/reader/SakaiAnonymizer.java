00d12b90e4551b1e61a5b706dc3d3a74efb096c7
Toby Dragon <dr.toby.dragon@gmail.com>
2018-11-16 17:12:38 -0500

    made anonymizer main

--- src/main/java/edu/ithaca/dragon/tecmap/io/reader/SakaiAnonymizer.java
+++ src/main/java/edu/ithaca/dragon/tecmap/io/reader/SakaiAnonymizer.java
@@ -1,5 +1,10 @@
 package edu.ithaca.dragon.tecmap.io.reader;
 
+import edu.ithaca.dragon.tecmap.Settings;
+import edu.ithaca.dragon.tecmap.io.Json;
+
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.*;
 
@@ -39,8 +44,44 @@ public class SakaiAnonymizer {
     }
 
     public static void main(String[] args){
+        //create anonymizer, either new or from old file
+        SakaiAnonymizer anonymizer;
+        try {
+            anonymizer = Json.fromJsonFile(
+                    "src/main/resources/anonHere/anonymizer.json", SakaiAnonymizer.class);
+        } catch (IOException e) {
+            anonymizer = new SakaiAnonymizer();
+        }
+        //anonymize all files in folder
+        try {
+            for (String filename : allCsvFilesInDirectory("src/main/resources/anonHere/")) {
+                if (!filename.contains("anon")) {
+                    anonymizer.anonymize("src/main/resources/anonHere/"+filename,
+                            "src/main/resources/anonHere/"+filename.substring(0, filename.length() - 4) + "-anon.csv");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //write new anonymizer file
+        try {
+            Json.toJsonFile("src/main/resources/anonHere/anonymizer.json", anonymizer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
+    }
+
+    private static List<String> allCsvFilesInDirectory(String directory) {
+        List<String> textFiles = new ArrayList<>();
+        File dir = new File(directory);
+        for (File file : dir.listFiles()) {
+            if (file.getName().toLowerCase().endsWith((".csv"))) {
+                textFiles.add(file.getName());
+            }
+        }
+        return textFiles;
     }
 
     public int getNumToUseNext() {
