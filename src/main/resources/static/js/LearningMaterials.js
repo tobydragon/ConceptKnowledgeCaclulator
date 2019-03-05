878d01969108eadc3600cc00c4517d804a57fa76
Phernandoe <fbarreto@ithaca.edu>
2019-03-05 15:43:05 -0500

    LearningMaterial.html displays LearningMaterialRecords as a list, without the tags. Also added bootstrap and jquery scripts

--- src/main/resources/static/js/LearningMaterials.js
+++ src/main/resources/static/js/LearningMaterials.js
@@ -1,12 +1,17 @@
 const file = "/api/learningMaterials";
 const records = readJson(file);
-let newheader;
-let newcontent
+const recordsList = document.getElementById("recordsList");
+let listItem;
+let content
+let tags;
 
 for (let i = 0; i < records.length; i++) {
-    newheader = document.createElement("h1");
-    newcontent = document.createTextNode(JSON.stringify(records[i]));
-    newheader.appendChild(newcontent);
-    document.body.appendChild(newheader);
+    listItem = document.createElement("li");
+    content = document.createTextNode(records[i].content);
+    // tags = document.createTextNode(Object.keys(records[i].tagsMap));
+    listItem.appendChild(content);
+    // newheader.appendChild(document.createTextNode(" "));
+    // newheader.appendChild(tags);
+    recordsList.appendChild(listItem);
 }
 
