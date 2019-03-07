d643450a286d1c9e2a5770af216126a1b5b53fc5
Phernandoe <fbarreto@ithaca.edu>
2019-03-07 13:11:59 -0500

    Added tags to LearningMaterials.html + some styling

--- src/main/resources/static/js/LearningMaterials.js
+++ src/main/resources/static/js/LearningMaterials.js
@@ -1,17 +1,23 @@
 const file = "/api/learningMaterials";
 const records = readJson(file);
 const recordsList = document.getElementById("recordsList");
+const tagsList = document.getElementById("tagsList");
 let listItem;
+let tagItem;
 let content
 let tags;
 
 for (let i = 0; i < records.length; i++) {
     listItem = document.createElement("li");
+    tagItem = document.createElement("li");
+
     content = document.createTextNode(records[i].content);
-    // tags = document.createTextNode(Object.keys(records[i].tagsMap));
+    tags = document.createTextNode(Object.keys(records[i].tagsMap));
+
+    tagItem.appendChild(tags);
     listItem.appendChild(content);
-    // newheader.appendChild(document.createTextNode(" "));
-    // newheader.appendChild(tags);
+
+    tagsList.appendChild(tagItem);
     recordsList.appendChild(listItem);
 }
 
