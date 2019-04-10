c241a6d9504d508196ac1f1ea49305c89056678c
Phernandoe <fbarreto@ithaca.edu>
2019-04-10 00:22:35 -0400

    Added styling for /connectMaterials & /connectMaterialsList

--- src/main/resources/static/js/ConnectMaterialsList.js
+++ src/main/resources/static/js/ConnectMaterialsList.js
@@ -3,14 +3,17 @@ var numOfLearningMaterials = learningMaterials.length;
 const recordsList = document.getElementById("recordsList");
 const nav = $("#index");
 var index = "0/";
+var listItem;
+var anchor;
+
 index = index.concat(numOfLearningMaterials);
 
 nav.text(index);
 
-
 for (var i = 0; i < numOfLearningMaterials; i++) {
     listItem = document.createElement("li");
-    content = document.createTextNode(learningMaterials[i].id);
-    listItem.appendChild(content);
+    anchor = document.createElement("a");
+    anchor.appendChild(document.createTextNode(learningMaterials[i].id));
+    listItem.appendChild(anchor);
     recordsList.appendChild(listItem);
 }
