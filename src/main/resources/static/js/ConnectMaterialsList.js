70e6d38e762a46edca5848cf60764a52e9e467bb
Phernandoe <fbarreto@ithaca.edu>
2019-04-24 01:57:49 -0400

    /connectMaterialList displays materials that have been linked

--- src/main/resources/static/js/ConnectMaterialsList.js
+++ src/main/resources/static/js/ConnectMaterialsList.js
@@ -1,8 +1,9 @@
 var materials = readJson("/api/connectMaterials/" + courseId);
+var resourceRecords = readJson("/api/currentResourceLinks/" + courseId);
 
 $(document).ready(function(){
     document.getElementById("recordsList").insertAdjacentHTML("beforebegin", "<h3>CourseID: " + courseId + "</h3>")
-    document.getElementById("recordsList").innerHTML = createListOfLearningRecordsString(materials);
+    document.getElementById("recordsList").innerHTML = createListOfLearningRecordsString(materials, resourceRecords);
     $("#index").text(loadNavString(materials.length));
 });
 
@@ -12,17 +13,32 @@ function loadNavString(numOfLearningMaterials){
     return index.concat(numOfLearningMaterials);
 }
 
-function createListOfLearningRecordsString(learningRecords) {
+function createListOfLearningRecordsString(materials, resourceRecords) {
 
     var typeString = "";
 
-    for (var i = 0; i < learningRecords.length; i++) {
-        typeString += "<li><a href=\"/view/connectMaterials/";
+    for (var i = 0; i < materials.length; i++) {
+        if (isMaterialLinked(materials[i].id, resourceRecords)) {
+            typeString += "<li><a class=\"linked\" href=\"/view/connectMaterials/";
+        } else {
+            typeString += "<li><a href=\"/view/connectMaterials/";
+        }
+
         typeString += courseId;
         typeString += "/" + i + "\">";
-        typeString += learningRecords[i].id;
-        typeString += "</a></li>";
+        typeString += materials[i].id;
+        typeString += " - Linked</a></li>";
     }
 
     return typeString;
+}
+
+function isMaterialLinked(materialID, records) {
+    for (var i = 0; i < records.length; i++){
+        if (materialID === records[i].learningResourceId){
+            return true;
+        }
+    }
+
+    return false;
 }
--- src/test/javascript/spec/ConnectMaterialsListSpec.js
+++ src/test/javascript/spec/ConnectMaterialsListSpec.js
@@ -3,13 +3,19 @@
 describe("ConnectMaterialsListSpecs", function() {
 
     var materials = readJson("../resources/datastore/Cs1Example/Cs1ExampleLearningMaterial.json");
+    var resourceRecords = readJson("../resources/datastore/Cs1Example/Cs1ExampleResources.json");
 
     it("updateNavString", function() {
         expect(loadNavString(10)).toEqual("0/10");
     });
 
     it("createListOfLearningRecordsString", function () {
-        expect(createListOfLearningRecordsString(materials)).toContain("HW2");
+        expect(createListOfLearningRecordsString(materials, resourceRecords)).toContain("HW2");
+    });
+
+    it("isMaterialLinked", function () {
+        expect(isMaterialLinked("Q1", resourceRecords)).toEqual(true);
+        expect(isMaterialLinked("Q10", resourceRecords)).toEqual(false);
     });
 
 });
